d650cd065b13cc03fe66c0980d29cc30d8ae9e07
Rodrigo Christofani <christofanir@gmail.com>
2024-08-21 17:30:50 -0300

    Iniciando projeto Agrix fase A, Req 1 finalizado.

---
+++ src/main/java/com/betrybe/agrix/controller/FarmController.java
@@ -0,0 +1,32 @@
+package com.betrybe.agrix.controller;
+
+import com.betrybe.agrix.controller.dto.FarmCreationDto;
+import com.betrybe.agrix.controller.dto.FarmDto;
+import com.betrybe.agrix.service.FarmService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/farms")
+public class FarmController {
+
+  private final FarmService farmService;
+
+  @Autowired
+  public FarmController(FarmService farmService) {
+    this.farmService = farmService;
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public FarmDto createFarm(@RequestBody FarmCreationDto farmCreationDto) {
+    return FarmDto.fromEntity(
+        farmService.create(farmCreationDto.toEntity())
+    );
+  }
+}
---
+++ src/main/java/com/betrybe/agrix/controller/dto/FarmDto.java
@@ -0,0 +1,18 @@
+package com.betrybe.agrix.controller.dto;
+
+import com.betrybe.agrix.entity.Farm;
+
+public record FarmDto(
+    Long id,
+    String name,
+    Double size
+) {
+
+  public static FarmDto fromEntity(Farm farm) {
+    return new FarmDto(
+        farm.getId(),
+        farm.getName(),
+        farm.getSize()
+    );
+  }
+}
